a8b85c17606e142c3dd46bff4f1cde12a38fce07
abishek <avineshmtp@gmail.com>
2024-12-16 16:59:39 +0530

    commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9000/api'; 
+const API_URL = 'https://mental-health-check-in-1.onrender.com/api'; 
 
 export const registerUser = async (userData) => {
     return await axios.post(`${API_URL}/auth/register`, userData);
